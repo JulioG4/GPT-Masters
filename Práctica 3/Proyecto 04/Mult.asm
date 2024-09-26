e2196ea378258ed68016a47feac5583efb6dc811
Juan Pablo Herrera Baquero <102593174+Juanherrera117@users.noreply.github.com>
2024-09-26 12:21:38 -0500

    Update Mult.asm

--- Práctica 3/Proyecto 04/Mult.asm
+++ Práctica 3/Proyecto 04/Mult.asm
@@ -1,1 +1,23 @@
+R1
+D=M        
+@END      
+D;JEQ
 
+(LOOP)
+  @R0
+  D=M     
+  @END   
+  D;JEQ
+
+  @R2
+  M=D+M    
+
+  @R1
+  M=M-1
+  D=M   
+  @LOOP    
+  D;JGT
+
+(END)
+  @END
+  0;JMP
